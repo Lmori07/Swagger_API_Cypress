6f335f1ad6386eba19d435bc388ec00ae0259a88
Jose Osv. Morillo Villar <joseomv07@gmail.com>
2023-06-14 11:56:13 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const low = require("lowdb");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const booksRouter = require("./routes/books");
+
+//Change port in order to no overlap any other application that is running on an specific port.
+//Make sure that when opening the port you are using the following url (http://localhost:{PORT}/api-docs/) where PORT is the actual port you entered.
+const PORT = process.env.PORT || 9000;
+
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+db.defaults({ books: [] }).write();
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API"
+    },
+    servers: [
+      {
+        url: "http://localhost:9000"
+      }
+    ]
+  },
+  apis: ["./routes/*.js"]
+};
+
+const specs = swaggerJsDoc(options);
+
+const app = express();
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+app.db = db;
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/books", booksRouter);
+
+app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
